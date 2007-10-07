011f87d691daab6373932d133905c5d65ef83836
cleber.goncalves <cleber.goncalves@61efdb93-8643-0410-af75-63d7a4e0ece5>
2007-10-07 12:23:44 +0000

    Stub for a test class for Vocabuilder

---
+++ test/br/boirque/vocabuilder/VocabuilderTest.java
@@ -0,0 +1,35 @@
+package br.boirque.vocabuilder;
+
+import junit.framework.TestCase;
+
+public class VocabuilderTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testStartApp() {
+		fail("Not yet implemented");
+	}
+
+	public void testPauseApp() {
+		fail("Not yet implemented");
+	}
+
+	public void testDestroyApp() {
+		fail("Not yet implemented");
+	}
+
+	public void testVocabuilder() {
+		fail("Not yet implemented");
+	}
+
+	public void testCommandAction() {
+		fail("Not yet implemented");
+	}
+
+}
