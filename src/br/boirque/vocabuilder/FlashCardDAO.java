c4379c580755a539fe8d56b14fe87ed22ab3e8e9
cleber.goncalves <cleber.goncalves@61efdb93-8643-0410-af75-63d7a4e0ece5>
2007-10-08 21:56:30 +0000

    Stub for DAO for the FlashCard Class

---
+++ src/br/boirque/vocabuilder/FlashCardDAO.java
@@ -0,0 +1,11 @@
+package br.boirque.vocabuilder;
+
+/**
+ * 
+ * @author cleber.goncalves
+ * Encapsulates the loading and saving of 
+ * flash card data to persistent state
+ */
+public class FlashCardDAO {
+	
+}
