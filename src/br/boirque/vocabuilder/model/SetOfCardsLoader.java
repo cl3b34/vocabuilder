ec6ea137002de70c2926fe8ee78c28b668a3957e
cleber.goncalves <cleber.goncalves@61efdb93-8643-0410-af75-63d7a4e0ece5>
2007-11-04 20:42:04 +0000

    2 new words on the mockset

--- src/br/boirque/vocabuilder/model/SetOfCardsLoader.java
+++ src/br/boirque/vocabuilder/model/SetOfCardsLoader.java
@@ -25,11 +25,16 @@ public class SetOfCardsLoader {
 		//just return a fake SetOfCards		
 		FlashCard mockCard1 = new FlashCard("tuli","fin","fire","eng",false,"burns");
 		FlashCard mockCard2 = new FlashCard("veta","fin","water","eng",false,"wets");
+		FlashCard mockCard3 = new FlashCard("leipa","fin","bread","eng",false,"is eatable");
+		FlashCard mockCard4 = new FlashCard("tietokone","fin","computer","eng",false,"knowledge machine");
+
 		
 		Vector cards = new Vector();
 		cards.addElement(mockCard1);
 		cards.addElement(mockCard2);
-		
+		cards.addElement(mockCard3);
+		cards.addElement(mockCard4);
+
 		SetOfCards mockSet = new SetOfCards("mockSet",false,2000L,cards);
 		return mockSet;
 	}
