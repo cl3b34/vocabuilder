43f47fd589433d1d39c3b22c47277e6b1f68a362
cleber.goncalves <cleber.goncalves@61efdb93-8643-0410-af75-63d7a4e0ece5>
2007-11-12 23:05:23 +0000

    reset the random indexer when reaching the end of the list

--- trunk/src/br/boirque/vocabuilder/controller/Initializer.java
+++ trunk/src/br/boirque/vocabuilder/controller/Initializer.java
@@ -14,24 +14,33 @@ import br.boirque.vocabuilder.model.SetOfCardsLoader;
 
 /**
  * Takes care of all the initialization tasks
+ * 
  * @author cleber.goncalves
- *
+ * 
  */
 public class Initializer {
-	
-	//In the future, this guy should check if there is any partially saved state available
+
+	// In the future, this guy should check if there is any partially saved
+	// state available
 	// (record stores in the hand set)
-	public SetOfCards initializeApp(){
-		//first try to load a partially studied set from a recordStore
+	public SetOfCards initializeApp() {
+		// first try to load a partially studied set from a recordStore
+
+		// TODO: remove this. Hack to remove the RS so we can
+		// start afresh. Should do better RS management
+		if(this.getRecordCount() > 150) {
+			this.resetState();
+		}
+
 		SetOfCards soc = this.loadState();
-		//TODO: the returned set might be marked as 'done'
+		// TODO: the returned set might be marked as 'done'
 		// we should ask the user what to do...
-		// either try to load another set from other media and 
+		// either try to load another set from other media and
 		// replace this or start studying the set again
-		// for now I just load another set. This code should reside in 
+		// for now I just load another set. This code should reside in
 		// the UI, not here (soc.isDone check)
-		if(soc == null || soc.isDone()) {
-			//Load the set from any available media (defined in the loader)
+		if (soc == null || soc.isDone()) {
+			// Load the set from any available media (defined in the loader)
 			SetOfCardsLoader socl = new SetOfCardsLoader();
 			try {
 				soc = socl.loadSet();
@@ -39,17 +48,17 @@ public class Initializer {
 				// TODO should send a message to the UI
 				e.printStackTrace();
 			}
-			//save the set to a local recordstore
-			//the return value of this method is ignored
-			//since it is only used for performance at this point
-			//this.saveState(soc);	//removed so the startup is faster		
+			// save the set to a local recordstore
+			// the return value of this method is ignored
+			// since it is only used for performance at this point
+			// this.saveState(soc); //removed so the startup is faster
 		}
 		return soc;
 	}
-	
+
 	/**
-	 * Save the current state of the set being studied to
-	 * persistent storage
+	 * Save the current state of the set being studied to persistent storage
+	 * 
 	 * @param soc
 	 * @return true if successfully saved the current state
 	 */
@@ -57,9 +66,9 @@ public class Initializer {
 		boolean savedSuccesfully = false;
 		try {
 			SetOfCardsDAO socDao = new SetOfCardsDAO();
-			//removes the previous recordstore
+			// removes the previous recordstore
 			socDao.resetState();
-			//create a new one with the current state
+			// create a new one with the current state
 			socDao.saveState(soc);
 			savedSuccesfully = true;
 		} catch (RecordStoreFullException e) {
@@ -78,12 +87,13 @@ public class Initializer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return savedSuccesfully;		
+		return savedSuccesfully;
 	}
-	
+
 	/**
-	 * Load the state of the set being currently studied from 
-	 * persistent storage.
+	 * Load the state of the set being currently studied from persistent
+	 * storage.
+	 * 
 	 * @return the currently studied set or null if a error occurred
 	 */
 	public SetOfCards loadState() {
@@ -109,5 +119,47 @@ public class Initializer {
 		}
 		return soc;
 	}
-	
+
+	/**
+	 * reset the state of the set being studied so we start fresh.
+	 */
+	public void resetState() {
+		try {
+			SetOfCardsDAO socDao = new SetOfCardsDAO();
+			socDao.resetState();
+		} catch (RecordStoreFullException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RecordStoreNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RecordStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * return the record count or -1 if an error occurs
+	 */
+	public int getRecordCount() {
+		try {
+			SetOfCardsDAO socDao = new SetOfCardsDAO();
+			return socDao.getRecordCount();
+		} catch (RecordStoreFullException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RecordStoreNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RecordStoreNotOpenException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RecordStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return -1;
+	}
+
 }
