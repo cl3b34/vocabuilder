494def072e9e6b7381ad0b28ebf938011abee55b
cl3b34 <cl3b34@61efdb93-8643-0410-af75-63d7a4e0ece5>
2008-04-05 10:48:55 +0000

    compiling English -> Swedish version.

--- trunk/src/br/boirque/vocabuilder/model/SetOfCardsLoader.java
+++ trunk/src/br/boirque/vocabuilder/model/SetOfCardsLoader.java
@@ -16,7 +16,7 @@ public class SetOfCardsLoader {
 
 	//load the default set
 	public SetOfCards loadSet() throws IOException {
-		return loadSet("/Swahilli/longlist_eng_swa.txt");
+		return loadSet("/Swedish/longlist_eng_swe.txt");
 	}
 	
 	// this method tries to load a set
