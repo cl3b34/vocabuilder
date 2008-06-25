3acb08cda1506389b2b37ec44fd0e5c915923fdd
cl3b34 <cl3b34@61efdb93-8643-0410-af75-63d7a4e0ece5>
2008-06-25 20:55:57 +0000

    Saving the last line. Making the last file larger

--- groovyUtils/src/FileSplitter.groovy
+++ groovyUtils/src/FileSplitter.groovy
@@ -22,95 +22,105 @@ class FileSplitter{
 					def limitChar = ""
 					def currentChar = ""
 					def splittedList = ""
-					lines.eachWithIndex{line,i ->	
-						if(line.startsWith("#")){
-							if(i==0){
-								//parse the file name
-								def underscore = line.indexOf("_")
-								def lang = line.substring(underscore,line.size())
-								filenameBase = "words" + lang
-							}
-							if(i==1){
-								sideOne = line
-							}
-							if(i==2){
-								sideTwo = line
-							}				
-						}else{
-							line = removeDuplicates(line)
-							//the first letter for the filename ending
-							if(i==3){firstCharOfList = line.getAt(0)} 
-							currentChar = line.getAt(0)
-							//we want around 400 words per file
-							if(i % 400 > 0){
-								if(limitChar.size() !=0 && !limitChar.equalsIgnoreCase(currentChar) ||i == lines.size()-1){
-									//	println "trocou " +limitChar + " " + currentChar
-									println "ending list ${filenameBase} at index ${i}"
-									
-									String filename = filenameBase+ "_"+ firstCharOfList + limitChar + ".txt"
-									title = "#setName = " + filename
-									meta = title + "\n" + sideOne + "\n" + sideTwo + "\n"
-								
-									String savingPath = file.getParent() + File.separator + filename
-									String contents = meta + splittedList
-									
-									//save
-									saveFile(contents, savingPath)
-									
-									//append this file to the properties
-									def propertiesFile = file.getParent() + File.separator + "properties.txt"
-									appendFile("defaultset="+filename + "\n" , propertiesFile)
-									
-									//zeroe the vars
-									firstCharOfList = currentChar
-									limitChar = ""
-									//save the newly loaded line to the next file
-									splittedList = line;
-
-								}else{
-									if(splittedList.size()==0){
-										splittedList = line
-									}else{							
-										splittedList = splittedList + "\n" + line
-									}
+					def propertiesTxt = ""
+					lines.eachWithIndex{line,i ->
+							if(line.startsWith("#")){
+								if(i==0){
+									//parse the file name
+									def underscore = line.indexOf("_")
+									def lang = line.substring(underscore,line.size())
+									filenameBase = "words" + lang
 								}
-								
+								if(i==1){
+									sideOne = line
+								}
+								if(i==2){
+									sideTwo = line
+								}				
 							}else{
-		//						400 cards, when the char the word starts changes, finish and save
-								limitChar = line.getAt(0)
-								splittedList = splittedList + "\n" + line
-								println "limit reached, look for next char after: " + limitChar
+								line = removeDuplicates(line)
+								//the first letter for the filename ending
+								if(i==3){firstCharOfList = line.getAt(0)} 
+								currentChar = line.getAt(0)
+								//we want around 400 words per file
+								if(i % 400 > 0){
+									//if we are close to the end of the list, just append it to the previous
+									if((lines.size() - i) < 150 && limitChar != "") {
+										println "lines size:" + lines.size() + "index: " + i
+										limitChar = ""
+									}
+									//save 
+									if(limitChar.size() !=0 && !limitChar.equalsIgnoreCase(currentChar) ||i == lines.size()-1){
+										if(i == lines.size()-1){
+											//save the last line of the file...
+											splittedList = splittedList + "\n" + line
+										}
+										//	println "trocou " +limitChar + " " + currentChar
+										println "ending list ${filenameBase} at index ${i}"
+										String setName = filenameBase+ "_"+ firstCharOfList + limitChar
+										
+										title = "#setName = " + setName
+										meta = title + "\n" + sideOne + "\n" + sideTwo + "\n"
+									
+										String filename =  setName + ".txt"
+										filename = filename.toLowerCase()
+										String savingPath = file.getParent() + File.separator + filename
+										String contents = meta + splittedList
+										
+										//save
+										saveFile(contents, savingPath)
+										
+										//append this file to the properties
+										propertiesTxt += "defaultset="+ filename + "\n"
+										//zeroe the vars
+										firstCharOfList = currentChar
+										limitChar = ""
+										//save the newly loaded line to the next file
+										splittedList = line;
+	
+									}else{
+										if(splittedList.size()==0){
+											splittedList = line
+										}else{							
+											splittedList = splittedList + "\n" + line
+										}
+									}
+									
+								}else{
+			//						400 cards, when the char the word starts changes, finish and save
+									limitChar = line.getAt(0)
+									splittedList = splittedList + "\n" + line
+//									println "limit reached, look for next char after: " + limitChar
+								}
 							}
-						}
 					}
-					//saves the last file
-					
-					
+					println "Finished file " + file.name
+					def propertiesFile = file.getParent() + File.separator + "properties.txt"
+					appendFile(propertiesTxt , propertiesFile)
 				}
-				
 			}
 		}
 	}
 	
 	void saveFile(String contents, String savingPath){
 		println "Saving" + savingPath
-		println()
+//		println()
 		File fileToSave = new File(savingPath)
 		fileToSave.write(contents)		
 	}
 	
 	
 	void appendFile(String contents, String savingPath){
-		println "Appending" + savingPath
+		println "Saving properties" + savingPath
 		println()
 		File fileToSave = new File(savingPath)
-		fileToSave.append(contents)		
+		fileToSave.write(contents)		
 	}
 	
 	//remove duplicated words
 	String removeDuplicates(String listToClean){
-		println "Removing duplicates "
-		println "before: " + listToClean
+//		println "Removing duplicates "
+//		println "before: " + listToClean
 		
 		List line = listToClean.tokenize("=")
 		String wordLeft = line.getAt(0)
@@ -126,7 +136,7 @@ class FileSplitter{
 				}
 			}
 			if(counter > 1){
-				println "found repeated: " + word + " " +counter + " times"
+//				println "found repeated: " + word + " " +counter + " times"
 				clean.remove(word)
 			}
 		}
@@ -138,8 +148,8 @@ class FileSplitter{
 			}
 		}
 		def cleanedList = wordLeft + "=" + wordsRight
-		println "after: " + cleanedList
-		println()
+//		println "after: " + cleanedList
+//		println()
 		return cleanedList
 	}
 	
